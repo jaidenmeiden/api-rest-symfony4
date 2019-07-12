6a27f215f5f9aaec9cb4c24d30ed4ce87748dc99
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-12 11:37:12 -0500

    Controladores y rutas en archivo externo .yaml

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,13 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    /**
-     * @Route("/user", name="user")
-     */
+
     public function index()
     {
         return $this->render('user/index.html.twig', [
--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -3,13 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
 class VideoController extends AbstractController
 {
-    /**
-     * @Route("/video", name="video")
-     */
     public function index()
     {
         return $this->render('video/index.html.twig', [
