7fee63a5dfc3fac026905f8da51c80edb0123cb5
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-12 14:52:42 -0500

    Se mapean las entidades

---
+++ src/Entity/Users.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Users
+ *
+ * @ORM\Table(name="users")
+ * @ORM\Entity
+ */
+class Users
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="name", type="string", length=45, nullable=true, options={"default"="NULL"})
+     */
+    private $name = 'NULL';
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="surname", type="string", length=45, nullable=true, options={"default"="NULL"})
+     */
+    private $surname = 'NULL';
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="email", type="string", length=255, nullable=true, options={"default"="NULL"})
+     */
+    private $email = 'NULL';
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="password", type="string", length=255, nullable=true, options={"default"="NULL"})
+     */
+    private $password = 'NULL';
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="role", type="string", length=45, nullable=true, options={"default"="NULL"})
+     */
+    private $role = 'NULL';
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="description", type="text", length=65535, nullable=true, options={"default"="NULL"})
+     */
+    private $description = 'NULL';
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="created_at", type="datetime", nullable=true, options={"default"="NULL"})
+     */
+    private $createdAt = 'NULL';
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="updated_at", type="datetime", nullable=true, options={"default"="NULL"})
+     */
+    private $updatedAt = 'NULL';
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="remember_token", type="string", length=255, nullable=true, options={"default"="NULL"})
+     */
+    private $rememberToken = 'NULL';
+
+
+}
---
+++ src/Entity/Videos.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Videos
+ *
+ * @ORM\Table(name="videos", indexes={@ORM\Index(name="fk_posts_users_idx", columns={"user_id"})})
+ * @ORM\Entity
+ */
+class Videos
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="title", type="string", length=45, nullable=true, options={"default"="NULL"})
+     */
+    private $title = 'NULL';
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="description", type="text", length=65535, nullable=true, options={"default"="NULL"})
+     */
+    private $description = 'NULL';
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="url", type="string", length=255, nullable=true, options={"default"="NULL"})
+     */
+    private $url = 'NULL';
+
+    /**
+     * @var int|null
+     *
+     * @ORM\Column(name="status", type="integer", nullable=true, options={"default"="NULL"})
+     */
+    private $status = 'NULL';
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="created_at", type="datetime", nullable=true, options={"default"="NULL"})
+     */
+    private $createdAt = 'NULL';
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="updated_at", type="datetime", nullable=true, options={"default"="NULL"})
+     */
+    private $updatedAt = 'NULL';
+
+    /**
+     * @var \Users
+     *
+     * @ORM\ManyToOne(targetEntity="Users")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     * })
+     */
+    private $user;
+
+
+}
