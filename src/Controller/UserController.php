01630b419bb47de0d5575f4c7e269e49a9577199
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-12 11:57:58 -0500

    Cambios básicos

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
-
     public function index()
     {
         return $this->render('user/index.html.twig', [
