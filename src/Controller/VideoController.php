a1a57bd15def65531bf5a03f42d626b6dff7ef07
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-15 16:50:16 -0500

    Guardar un video nuevo

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -58,12 +58,68 @@ class VideoController extends AbstractController
     }
 
     public function create(Request $request, JwtAuthService $jwt_auth_service) {
+        //Recoger la cabecera de autentificación}
+        $token = $request->headers->get('Authorization');
+        $checkToken = $jwt_auth_service->checkToken($token);
+
         //Array por defecto para devolver
         $data = [
             'status' => 'error',
             'code' => 400,
             'message' => 'El video no se ha creado'
         ];
+
+
+        //Si es correcto, hacer la actualización del usuario
+        if ($checkToken) {
+            //Recorger los datos por POST
+            $json = $request->get('json', null);
+            $params = json_decode($json);//Objeto
+
+            //Comprobar y validad datos
+            if(!empty($json)) {
+                //Conseguir los datos de usuario identificado
+                $identity = $jwt_auth_service->checkToken($token, true);
+
+                $user_id = (isset($identity->sub)) ? $identity->sub : null;
+                $title = (isset($params->title)) ? $params->title : null;
+                $description = (isset($params->description)) ? $params->description : null;
+                $url = (isset($params->url)) ? $params->url : null;
+
+                if(!empty($user_id) && !empty($title) && !empty($description) && !empty($url)) {
+                    //Conseguir el entity manager
+                    $doctrine = $this->getDoctrine();
+                    $em = $doctrine->getManager();
+
+                    $user_repo = $doctrine->getRepository(User::class);
+                    $user = $user_repo->findOneBy(array(
+                        'id' => $user_id
+                    ));
+
+                    //Si la validación es correcta, crear el objeto usuario
+                    $video = new Video();
+                    $video->setUser($user);
+                    $video->setTitle($title);
+                    $video->setDescription($description);
+                    $video->setUrl($url);
+                    $video->setCreatedAt(new \DateTime('now'));
+                    $video->setUpdatedAt(new \DateTime('now'));
+
+                    //Guardar usuario
+                    $em->persist($video);
+                    $em->flush();
+
+                    $data = [
+                        'status' => 'success',
+                        'code' => 200,
+                        'message' => 'Video creado correctamente',
+                        'video' => $video
+                    ];
+                } else {
+                    $data['message'] = 'Validación incorrecta';
+                }
+            }
+        }
 
         //Crear respuesta en JSON
         return $this->responseJsonPersonalizado($data);
